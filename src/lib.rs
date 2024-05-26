13ea1f9d92a530644223a0e7257918d8919977e0
Alessio Gravili <alessio@gravili.de>
2024-05-25 23:28:07 -0400

    chore: switch to rust nightly and simplify + split up code. Use rust matching to simplify code

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(box_patterns)]
+
 use std::collections::HashSet;
 use serde::Deserialize;
 use swc_core::atoms::JsWord;
@@ -11,97 +13,6 @@ use swc_core::ecma::visit::VisitMutWith;
 use swc_core::plugin::{plugin_transform, proxies::TransformPluginProgramMetadata};
 
 
-
-
-
-// Helper function to transform exports
-fn transform_exports(module: &mut Module, exported_identifiers: &HashSet<JsWord>) {
-    let mut items_to_remove = Vec::new();
-
-    for item in &mut module.body {
-        match item {
-            ModuleItem::ModuleDecl(ModuleDecl::ExportDecl(ref mut export)) => {
-                match &mut export.decl {
-                    Decl::Var(var) => {
-                        for decl in &mut var.decls {
-                            if let Some(init) = &mut decl.init {
-                                if let Expr::Arrow(_) | Expr::Fn(_) = &**init {
-                                    *init = Box::new(Expr::Arrow(ArrowExpr {
-                                        span: DUMMY_SP,
-                                        params: vec![],
-                                        body: Box::new(BlockStmtOrExpr::Expr(Box::new(Expr::Lit(Lit::Null(Null { span: DUMMY_SP }))))),
-                                        is_async: false,
-                                        is_generator: false,
-                                        return_type: None,
-                                        type_params: None,
-                                    }));
-                                } else {
-                                    *init = Box::new(Expr::Lit(Lit::Null(Null { span: DUMMY_SP })));
-                                }
-                            } else {
-                                decl.init = Some(Box::new(Expr::Lit(Lit::Null(Null { span: DUMMY_SP }))));
-                            }
-                        }
-                    },
-                    Decl::Fn(func) => {
-                        func.function.body = Some(BlockStmt {
-                            span: DUMMY_SP,
-                            stmts: vec![Stmt::Return(ReturnStmt {
-                                span: DUMMY_SP,
-                                arg: Some(Box::new(Expr::Lit(Lit::Null(Null { span: DUMMY_SP })))),
-                            })],
-                        });
-                    },
-                    _ => {}
-                }
-            },
-            ModuleItem::Stmt(Stmt::Decl(Decl::Var(var))) => {
-                var.decls.retain_mut(|decl| {
-                    if let Pat::Ident(BindingIdent { id: Ident { sym, .. }, .. }) = &decl.name {
-                        if !exported_identifiers.contains(sym) {
-                            // Mark this variable declaration for removal
-                            items_to_remove.push(sym.clone());
-                            return false;
-                        } else {
-                            if let Some(init) = &mut decl.init {
-                                if let Expr::Arrow(_) | Expr::Fn(_) = &**init {
-                                    *init = Box::new(Expr::Arrow(ArrowExpr {
-                                        span: DUMMY_SP,
-                                        params: vec![],
-                                        body: Box::new(BlockStmtOrExpr::Expr(Box::new(Expr::Lit(Lit::Null(Null { span: DUMMY_SP }))))),
-                                        is_async: false,
-                                        is_generator: false,
-                                        return_type: None,
-                                        type_params: None,
-                                    }));
-                                } else {
-                                    *init = Box::new(Expr::Lit(Lit::Null(Null { span: DUMMY_SP })));
-                                }
-                            } else {
-                                decl.init = Some(Box::new(Expr::Lit(Lit::Null(Null { span: DUMMY_SP }))));
-                            }
-                        }
-                    }
-                    true
-                });
-            },
-            _ => {}
-        }
-    }
-
-    // Remove marked items
-    module.body.retain(|item| {
-        if let ModuleItem::Stmt(Stmt::Decl(Decl::Var(var))) = item {
-            return var.decls.iter().any(|decl| {
-                if let Pat::Ident(BindingIdent { id: Ident { sym, .. }, .. }) = &decl.name {
-                    return !items_to_remove.contains(&sym);
-                }
-                true
-            });
-        }
-        true
-    });
-}
 
 
 #[derive(Debug, Deserialize)]
@@ -149,114 +60,46 @@ impl VisitMut for TransformVisitor {
         return
     }
 
-
-    fn visit_mut_program(&mut self, p: &mut Program) {
-        p.visit_mut_children_with(self);
+    ///
+    /// This is used to lobotomize client components (so, if 'use client' is at the top of the file), by making sure it only contains stuff which is exported, and that those exports only return null
+    ///
+    fn visit_mut_program(&mut self, program: &mut Program) {
+        program.visit_mut_children_with(self);
 
         if !self.lobotomize_use_client_files {
             return;
         }
 
-        if let Program::Module(ref mut module) = p {
+        if let Program::Module(ref mut module) = program {
             // Check for "use client" declaration at the top
-            let mut has_use_client = false;
-            for item in &module.body {
-                if let ModuleItem::Stmt(Stmt::Expr(ExprStmt { expr, .. })) = item {
-                    if let Expr::Lit(Lit::Str(Str { value, .. })) = &**expr {
-                        if value.trim().eq_ignore_ascii_case("use client") {
-                            has_use_client = true;
-                            break;
-                        }
-                    }
-                }
-            }
 
-            if has_use_client {
-                // Preserve "use client" and strip imports, except re-exported ones
-                let mut reexported_imports = HashSet::new();
+            let contains_use_client = module.body.iter().any(|item| {
+                matches!(item, ModuleItem::Stmt(Stmt::Expr(ExprStmt { expr: box Expr::Lit(Lit::Str(Str { value, .. })), .. })) if value.trim().eq_ignore_ascii_case("use client"))
+            });
 
+            if contains_use_client {
                 // Collect re-exported imports
-                for item in &module.body {
-                    if let ModuleItem::ModuleDecl(ModuleDecl::ExportNamed(export)) = item {
-                        for specifier in &export.specifiers {
-                            if let ExportSpecifier::Named(ExportNamedSpecifier { orig, exported, .. }) = specifier {
-                                if let Some(ModuleExportName::Ident(ident)) = exported {
-                                    reexported_imports.insert(ident.sym.clone());
-                                } else if let ModuleExportName::Ident(ident) = orig {
-                                    reexported_imports.insert(ident.sym.clone());
-                                }
-                            }
-                        }
-                    }
-                }
+                let mut reexported_imports = collect_reexported_imports(module);
+
 
                 // Filter module body to strip imports and keep necessary parts
-                module.body.retain(|item| {
-                    match item {
-                        ModuleItem::Stmt(Stmt::Expr(ExprStmt { expr, .. })) => {
-                            if let Expr::Lit(Lit::Str(Str { value, .. })) = &**expr {
-                                if value.trim().eq_ignore_ascii_case("use client") {
-                                    return true;
-                                }
-                            }
+                module.body.retain(|item| match item {
+                    ModuleItem::Stmt(Stmt::Expr(ExprStmt { expr: box Expr::Lit(Lit::Str(Str { value, .. })), .. })) if value.trim().eq_ignore_ascii_case("use client") => true,
+                    ModuleItem::ModuleDecl(ModuleDecl::Import(import)) => import.specifiers.iter().any(|specifier| {
+                        match specifier {
+                            ImportSpecifier::Named(named) => reexported_imports.contains(&named.local.sym),
+                            ImportSpecifier::Default(default) => reexported_imports.contains(&default.local.sym),
+                            ImportSpecifier::Namespace(namespace) => reexported_imports.contains(&namespace.local.sym),
                         }
-                        ModuleItem::ModuleDecl(ModuleDecl::Import(import)) => {
-                            // Retain import if it is re-exported
-                            return import.specifiers.iter().any(|specifier| {
-                                match specifier {
-                                    ImportSpecifier::Named(named) => {
-                                        reexported_imports.contains(&named.local.sym)
-                                    }
-                                    ImportSpecifier::Default(default) => {
-                                        reexported_imports.contains(&default.local.sym)
-                                    }
-                                    ImportSpecifier::Namespace(namespace) => {
-                                        reexported_imports.contains(&namespace.local.sym)
-                                    }
-                                }
-                            });
-                        }
-                        ModuleItem::ModuleDecl(ModuleDecl::ExportNamed(export)) => {
-                            // Remove named exports that are not directly re-exporting
-                            return export.specifiers.iter().any(|specifier| {
-                                if let ExportSpecifier::Named(ExportNamedSpecifier { orig, exported, .. }) = specifier {
-                                    if let Some(ModuleExportName::Ident(ident)) = exported {
-                                        return reexported_imports.contains(&ident.sym);
-                                    } else if let ModuleExportName::Ident(ident) = orig {
-                                        return reexported_imports.contains(&ident.sym);
-                                    }
-                                }
-                                false
-                            });
-                        }
-                        _ => {}
-                    }
-                    true
+                    }),
+                    ModuleItem::ModuleDecl(ModuleDecl::ExportNamed(export)) => export.specifiers.iter().any(|specifier| {
+                        matches!(specifier, ExportSpecifier::Named(ExportNamedSpecifier { orig: ModuleExportName::Ident(ident), .. }) if reexported_imports.contains(&ident.sym))
+                    }),
+                    _ => true,
                 });
 
-                // Collect all identifiers that are exported
-                let mut exported_identifiers = HashSet::new();
-                for item in &module.body {
-                    if let ModuleItem::ModuleDecl(ModuleDecl::ExportNamed(export)) = item {
-                        for specifier in &export.specifiers {
-                            if let ExportSpecifier::Named(ExportNamedSpecifier { orig, .. }) = specifier {
-                                let ident = match orig {
-                                    ModuleExportName::Ident(ident) => ident.sym.clone(),
-                                    ModuleExportName::Str(_) => continue,
-                                };
-                                exported_identifiers.insert(ident);
-                            }
-                        }
-                    } else if let ModuleItem::ModuleDecl(ModuleDecl::ExportDecl(ExportDecl { decl, .. })) = item {
-                        if let Decl::Var(var) = decl {
-                            for decl in &var.decls {
-                                if let Pat::Ident(BindingIdent { id: Ident { sym, .. }, .. }) = &decl.name {
-                                    exported_identifiers.insert(sym.clone());
-                                }
-                            }
-                        }
-                    }
-                }
+                // Collect exported identifiers
+                let exported_identifiers = collect_exported_identifiers(module);
 
                 // Transform the module to retain and modify exported items
                 transform_exports(module, &exported_identifiers);
@@ -266,6 +109,135 @@ impl VisitMut for TransformVisitor {
     }
 
 }
+
+fn collect_reexported_imports(module: &Module) -> HashSet<JsWord> {
+    let mut reexported_imports = HashSet::new();
+    for item in &module.body {
+        if let ModuleItem::ModuleDecl(ModuleDecl::ExportNamed(export)) = item {
+            for specifier in &export.specifiers {
+                if let ExportSpecifier::Named(ExportNamedSpecifier { orig: ModuleExportName::Ident(ident), .. }) = specifier {
+                    reexported_imports.insert(ident.sym.clone());
+                }
+            }
+        }
+    }
+    reexported_imports
+}
+
+fn collect_exported_identifiers(module: &Module) -> HashSet<JsWord> {
+    let mut exported_identifiers = HashSet::new();
+    for item in &module.body {
+        if let ModuleItem::ModuleDecl(ModuleDecl::ExportNamed(export)) = item {
+            for specifier in &export.specifiers {
+                if let ExportSpecifier::Named(ExportNamedSpecifier { orig: ModuleExportName::Ident(ident), .. }) = specifier {
+                    exported_identifiers.insert(ident.sym.clone());
+                }
+            }
+        } else if let ModuleItem::ModuleDecl(ModuleDecl::ExportDecl(ExportDecl { decl: Decl::Var(var), .. })) = item {
+            for decl in &var.decls {
+                if let Pat::Ident(BindingIdent { id: Ident { sym, .. }, .. }) = &decl.name {
+                    exported_identifiers.insert(sym.clone());
+                }
+            }
+        }
+    }
+    exported_identifiers
+}
+
+
+/// Transforms the module by modifying exported variable initializers to `null`
+/// or an empty function returning `null`, and removes variable declarations
+/// that are not exported.
+///
+/// This function performs the following steps:
+/// 1. Iterates through the module items and processes export declarations to
+///    transform their initializers.
+/// 2. Transforms standalone variable declarations to ensure they are exported,
+///    marking those that are not for removal.
+/// 3. Removes variable declarations that were marked for removal.
+///
+/// # Parameters
+/// - `module`: The module to transform.
+/// - `exported_identifiers`: A set of identifiers that are exported.
+fn transform_exports(module: &mut Module, exported_identifiers: &HashSet<JsWord>) {
+    let mut items_to_remove = Vec::new();
+
+    for item in &mut module.body {
+        match item {
+            // Transform export declarations
+            ModuleItem::ModuleDecl(ModuleDecl::ExportDecl(ref mut export)) => {
+                if let Decl::Var(var) = &mut export.decl {
+                    for decl in &mut var.decls {
+                        transform_decl_init(decl);
+                    }
+                } else if let Decl::Fn(func) = &mut export.decl {
+                    func.function.body = Some(empty_function_body());
+                }
+            },
+            // Transform standalone variable declarations
+            ModuleItem::Stmt(Stmt::Decl(Decl::Var(var))) => {
+                var.decls.retain_mut(|decl| {
+                    if let Pat::Ident(BindingIdent { id: Ident { sym, .. }, .. }) = &decl.name {
+                        if !exported_identifiers.contains(sym) {
+                            // Mark this variable declaration for removal
+                            items_to_remove.push(sym.clone());
+                            return false;
+                        } else {
+                            transform_decl_init(decl);
+                        }
+                    }
+                    true
+                });
+            },
+            _ => {}
+        }
+    }
+
+    // Remove marked items
+    module.body.retain(|item| {
+        if let ModuleItem::Stmt(Stmt::Decl(Decl::Var(var))) = item {
+            return var.decls.iter().any(|decl| {
+                if let Pat::Ident(BindingIdent { id: Ident { sym, .. }, .. }) = &decl.name {
+                    return !items_to_remove.contains(&sym);
+                }
+                true
+            });
+        }
+        true
+    });
+}
+
+fn transform_decl_init(decl: &mut VarDeclarator) {
+    if let Some(init) = &mut decl.init {
+        if matches!(**init, Expr::Arrow(_) | Expr::Fn(_)) {
+            *init = Box::new(Expr::Arrow(ArrowExpr {
+                span: DUMMY_SP,
+                params: vec![],
+                body: Box::new(BlockStmtOrExpr::Expr(Box::new(Expr::Lit(Lit::Null(Null { span: DUMMY_SP }))))),
+                is_async: false,
+                is_generator: false,
+                return_type: None,
+                type_params: None,
+            }));
+        } else {
+            *init = Box::new(Expr::Lit(Lit::Null(Null { span: DUMMY_SP })));
+        }
+    } else {
+        decl.init = Some(Box::new(Expr::Lit(Lit::Null(Null { span: DUMMY_SP }))));
+    }
+}
+
+fn empty_function_body() -> BlockStmt {
+    BlockStmt {
+        span: DUMMY_SP,
+        stmts: vec![Stmt::Return(ReturnStmt {
+            span: DUMMY_SP,
+            arg: Some(Box::new(Expr::Lit(Lit::Null(Null { span: DUMMY_SP })))),
+        })],
+    }
+}
+
+
 
 
 #[plugin_transform]
